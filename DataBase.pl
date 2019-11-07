bd9ea9b5e5f171dd73f7b36ee3ff346558a375e8
davepj007 <davepj07@gmail.com>
2019-11-07 00:22:56 -0600

    Actualizacion Base de Datos
    Se mejoraron las funciones de asignar y liberar pista, asi como los hechos de las pistas agregando un espacio para la placa. Ademas se implemento una funcion para liberar todas las pistas.

--- DataBase.pl
+++ DataBase.pl
@@ -8,13 +8,13 @@ aeronave(mediana, [boeing717, embraer190, airBusA220]).
 aeronave(grande, [boeing747, airBusA340, airBusA380]).
 
 % Tabla Pistas
-% pista(numero, largo, tipoNave, direccion, disponibilidad, horaOcup)
+% pista(numero, largo, tipoNave, direccion, disponibilidad, Placa)
 
-:-dynamic pista/6.
-pista(p1, "1km", [pequena], [ns, eo, oe, sn], na, 0).
-pista(p2-1, "2km", [pequena, mediana], [eo], na, 0).
-pista(p2-2, "2km", [pequena, mediana], [oe], na, 0).
-pista(p3, "3km", [pequena, mediana, grande], [ns, eo, oe, sn], na, 0).
+:-dynamic pista/7.
+pista(p1, "1km", [pequena], [ns, eo, oe, sn], na, 0, "").
+pista(p2-1, "2km", [pequena, mediana], [eo], na, 0, "").
+pista(p2-2, "2km", [pequena, mediana], [oe], na, 0, "").
+pista(p3, "3km", [pequena, mediana, grande], [ns, eo, oe, sn], na, 0, "").
 
 % Tabla Emergencias
 
@@ -42,18 +42,35 @@ tipoNave(X, Nave):-
     aeronave(X, Y),
     miembro(Nave, Y).
 
-asignarPista(Nave, HoraAsig, Dir):-
+asignarPista(Nave, HoraAsig, Dir, Placa):-
     tipoNave(TNave,Nave),
-    pista(Pista, _, Tams, Dirs, na, _),
+    pista(Pista, _, Tams, Dirs, na, _, _),
     miembro(TNave, Tams), miembro(Dir, Dirs),
     write(Pista),
-    assert(pista(Pista, _, Tams, Dir, Nave, HoraAsig)),
-    retract(pista(Pista, _, Tams, _, na, 0)).
+    assert(pista(Pista, _, Tams, Dirs, Nave, HoraAsig, Placa)),
+    retract(pista(Pista, _, Tams, _, na, 0, "")).
+
+asignarPista(Nave, HoraAsig, Dir, Placa):-
+    tipoNave(TNave,Nave),
+    pista(Pista, _, Tams, Dirs, _, HoraAsig, _),
+    miembro(TNave, Tams), miembro(Dir, Dirs),
+    NHoraAsig is HoraAsig + 5,
+    write("Se asigno la pista "), write(Pista),
+    write(" para usar a las "), write(NHoraAsig),
+    assert(pista(Pista, _, Tams, Dirs, Nave, NHoraAsig, Placa)),
+    retract(pista(Pista, _, Tams, Dirs, _, HoraAsig, _)).
 
 
-liberarPista(Nave, HoraAsig, Dir):-
+liberarPista(Nave, HoraAsig, Dir, Placa):-
     tipoNave(TNave, Nave),
-    pista(Pista, _, Tams, _, Nave, HoraAsig),
-    miembro(TNave, Tams),
-    assert(pista(Pista, _, Tams, Dir, na, 0)),
-    retract(pista(Pista, _, Tams, Dir, Nave, HoraAsig)).
+    pista(Pista, _, Tams, Dirs, Nave, HoraAsig, Placa),
+    miembro(TNave, Tams), miembro(Dir, Dirs),
+    assert(pista(Pista, _, Tams, Dirs, na, 0, "")),
+    retract(pista(Pista, _, Tams, Dirs, Nave, HoraAsig, Placa)).
+
+liberarTodo:-
+    retractall(pista(_,_,_,_,_,_,_)),
+    assert(pista(p1, "1km", [pequena], [ns, eo, oe, sn], na, 0, "")),
+    assert(pista(p2-1, "2km", [pequena, mediana], [eo], na, 0, "")),
+    assert(pista(p2-2, "2km", [pequena, mediana], [oe], na, 0, "")),
+    assert(pista(p3, "3km", [pequena, mediana, grande], [ns, eo, oe, sn], na, 0, "")).
